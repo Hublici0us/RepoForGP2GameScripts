f5ffa7db928b4e9528799e6d22e4236ef839c72e
Jason Ky <145072420+Hublici0us@users.noreply.github.com>
2025-03-27 11:44:46 -0700

    e

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+public class Humanoid : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+
+    public float speed;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+}
+
+public class Enemy : Humanoid
+{
+   
+}
+
+public class Player : Humanoid
+{
+
+}
